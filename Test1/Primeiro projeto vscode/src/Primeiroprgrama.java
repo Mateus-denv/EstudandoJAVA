bf00ef4ed4fbfecef5e8e8ad9ed2aa7355677295
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-04 11:08:00 -0300

    Olá, mundo / Hello, Word

---
+++ Test1/Primeiro projeto vscode/src/Primeiroprgrama.java
@@ -0,0 +1,6 @@
+// Pacote
+public class Primeiroprgrama { // Class é um bloco
+    public static void main (String[] args){ // Metodo pricipal
+        System.out.println("Ola, mundo"); // Imprimir uma informação na tela
+    }
+}
